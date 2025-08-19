9596b890c608502ad1b988454f30c7cff5cd2894
hacktiwist <xurshidbekmbs@gmai.com>
2025-08-19 09:09:32 +0500

    charts

--- app/Http/Controllers/DashboardsController.php
+++ app/Http/Controllers/DashboardsController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Charts\CpuBusy;
 
 class DashboardsController extends Controller
 {
-    public function system(CpuBusy $cpubusy){
-        return view('dashboards.system',['cpubusy'=>$cpubusy]);
+    public function system(){
+        return view('dashboards.system');
     }
 }
--- resources/views/dashboards/system.blade.php
+++ resources/views/dashboards/system.blade.php
@@ -2,21 +2,81 @@
 
 
 
+@section('style')
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+@endsection
+
 @section('content')
 
 
-<div style="width: 350px;">
-    {!! $cpubusy->container() !!}
+<div class="row">
+    <div class="col-3">
+        <label for="cpu_busy" class="text-center">RAM</label>
+        <div id="cpu_busy"></div>
+    </div>
+</div>
+
+<br>
+
+<div class="row">
+     <div class="col-12">
+        <label for="cpu_busy" class="text-center">CPU load</label>
+        <div id="cpuLoadChart"></div>
+    </div>
 </div>
 
 @endsection
 
-
-
 @section('script')
 
+<script>
+    axios.get('/api/metrics')
+    .then(function (response) {
+        let metrics = response.data;
 
-<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-{!! $cpubusy->script() !!}
+        // RAM donut chart
+        var options = {
+            series: [metrics.ram_used.used_gb, metrics.ram_used.free_gb],
+            chart: {
+                type: 'donut',
+            },
+            labels: ['Band RAM (GB)', 'Bo‘sh RAM (GB)'],
+            responsive: [{
+                breakpoint: 480,
+                options: {
+                    chart: { width: 200 },
+                    legend: { position: 'bottom' }
+                }
+            }]
+        };
+
+        var chart = new ApexCharts(document.querySelector("#cpu_busy"), options);
+        chart.render();
+
+
+        // CPU Load line chart
+        var load_options = {
+            series: [{
+                name: "Load Average",
+                data: [
+                    metrics.sysload.load_1min,
+                    metrics.sysload.load_5min,
+                    metrics.sysload.load_15min
+                ]
+            }],
+            chart: { type: 'line', height: 300 },
+            xaxis: { categories: ['1 min', '5 min', '15 min'] }
+        };
+
+        var load = new ApexCharts(document.querySelector("#cpuLoadChart"), load_options);
+        load.render();
+    })
+    .catch(function (error) {
+        console.error("API error:", error);
+    });
+
+
+</script>
 
 @endsection
